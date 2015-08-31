e116165e802e06c6e252ec4d221de219a509e3c4
ikanyu <wongwy93@gmail.com>
2015-08-31 18:45:37 +0800

    add root address

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # resources :serimayas, only: [:index] => 'serimaya'
+  root 'serimayas#index'
   get 'serimaya', to: 'serimayas#index'
   get 'prima16', to: 'prima16s#index'
 end
