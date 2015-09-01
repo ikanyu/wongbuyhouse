d2c8451bb987371f2987e6bf0852a7e06e6fe2c2
ikanyu <wongwy93@gmail.com>
2015-09-02 01:52:45 +0800

    change code to make it faster

--- app/controllers/serimayas_controller.rb
+++ app/controllers/serimayas_controller.rb
@@ -1,74 +1,56 @@
 require 'date'
 class SerimayasController < ApplicationController
 	def index
-		@items = Array.new
-		@prices = Array.new
-		@parent = Hash.new
+		@temp = []
+		link = []
+		@temp1 = Hash.new
 		pagecounter = 1
-		ncounter = 1
 		starts = (Date.today - 31)
-		
 		counter = 1
-		while pagecounter < 10
-			page = Nokogiri::HTML(open("http://www.propwall.my/setiawangsa/seri_maya/24?tab=classifieds&page=#{pagecounter}"))
-			@items = page.css('.media-body').text.split('Info')
-			while counter <= @items.length
-				@total = page.css('div.media:nth-child(' + counter.to_s + ') > div:nth-child(2) > div:nth-child(2) > div:nth-child(1)').text.split(' ')
-				@parent[ncounter] = Hash.new
-				@by = @total.index('by')
-				@on = @total.index('on')
-				@parent[ncounter]['name'] = @total[(@by+1)..(@on-1)].join(' ')
-				if @total[(@on+1)][10] == "*"
-					@parent[ncounter]['bed'] = @total[(@on+1)][11]
-				else
-					@parent[ncounter]['bed'] = @total[(@on+1)][10]
-				end
-				if @total[@on+1].include?("Fully")
-					@parent[ncounter]['furnish'] = "Fully"
-				elsif @total[@on+1].include?("Semi")
-					@parent[ncounter]['furnish'] = "Semi"
-				else	
-					@parent[ncounter]['furnish'] = "No"
-				end
-				@parent[ncounter]['size'] = @total[-2]
-				@price = page.css('div.media:nth-child(' + counter.to_s + ') > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > p:nth-child(1)')
-				link = page.css('div.media:nth-child(' + counter.to_s + ') > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > p:nth-child(1) > a:nth-child(1)')
-				@parent[ncounter]['link'] = link.map { |link| link['href'] }.join
-				@parent[ncounter]['price'] = @price.text.split(' ').second.gsub(/,/, '').to_i
-				if Date.parse(@total[(@on+1)][0..9]) > starts
-					@parent[ncounter]['date'] = @total[(@on+1)][0..9]
-				else 
-					@parent.delete(ncounter)	
-				end
-				counter += 1
-				ncounter += 1
-				
-			end
-			if counter == 31
-				counter = 1
-			end
+		while pagecounter <= 10
+			@temp << Nokogiri::HTML(open("http://www.propwall.my/setiawangsa/seri_maya/24?tab=classifieds&page=#{pagecounter}")).css('.media-body').text.split(' Info')
+			link << Nokogiri::HTML(open("http://www.propwall.my/setiawangsa/seri_maya/24?tab=classifieds&page=#{pagecounter}")).css('.media-heading a').map{|link| link['href']}
 			pagecounter += 1
-			
+		end
+		@temp.each_with_index do |t,t_index|
+			t.each_with_index do |inner,i_index|
+				inner = inner.split(' ')
+				# byebug
+				inner.delete("Seri")
+				inner.delete("Maya,")
+				inner.delete("SetiawangsaPosted")
+				inner.delete("by")
+				# inner.delete("on")
+				inner.delete("Furnished")
+				inner.delete("Condominium")
+				inner.delete("for")
+				inner.delete("sale")
+				inner.delete("#")
+				inner.delete("sfRM")
+				inner.delete("(RM")
+				inner.delete("psf)Contact")
+				inner.delete("|")
+				inner.delete("More")
+				inner.delete("InfoSeri")
+				if Date.parse(inner[(inner.index('on')+1)][0..9]) > starts
+					@temp1[counter] = Hash.new
+					@temp1[counter]['name'] = inner[0..(inner.index('on')-1)].join(' ')
+					@temp1[counter]['date'] = inner[(inner.index('on')+1)][0..9]
+					@temp1[counter]['size'] = inner[-3]
+					@temp1[counter]['price'] = (inner[-2]).gsub(/,/, '').to_i
+					@temp1[counter]['link'] = link[t_index][i_index]
+					inner[(inner.index('on')+1)][10] != "*" ? @temp1[counter]['bed'] = inner[(inner.index('on')+1)][10] : @temp1[counter]['bed'] = inner[(inner.index('on')+1)][11]
+					if inner[(inner.index('on')+1)].include?("Fully")
+						@temp1[counter]['furnish'] = "Fully"
+					elsif inner[(inner.index('on')+1)].include?("Semi")
+						@temp1[counter]['furnish'] = "Semi"
+					else	
+						@temp1[counter]['furnish'] = "No"
+					end
+					counter+=1
+				end
+			end
 		end	
-		@parent = @parent.sort_by {|key, value| value['price']}.to_h
-
+		@temp1 = @temp1.sort_by {|key, value| value['price']}.to_h
 	end
 end
-
-# #person
-# div.media:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1)
-# div.media:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1)
-# #fully furnished?
-# div.media:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1)
-# div.media:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1)
-
-
-# #price pattern
-# div.media:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > p:nth-child(1)
-# div.media:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > p:nth-child(1)
-
-
-
-# #no. of rooms
-# div.media:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2)
-# div.media:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2)
